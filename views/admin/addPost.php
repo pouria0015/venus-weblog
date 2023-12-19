e7dd3b4088ad685870db584811217570a510f8ca
pouria0015 <pouria0015@gmail.com>
2023-12-19 23:05:21 +0330

    add: add ckeditor for text post

--- views/admin/addPost.php
+++ views/admin/addPost.php
@@ -1,42 +1,45 @@
-<?php 
-    view("admin/inc/header");
+<?php
+view("admin/inc/header");
 ?>
+<script src="<?= asset('js/ckeditor/ckeditor.js') ?>"></script>
 
-        <div class="d-flex justify-content-center">
-            <div class="row row-cols-1 row-cols-md-1 g-1">
+<div class="d-flex justify-content-center">
+    <div class="row row-cols-1 row-cols-md-1 g-1">
 
-                <div class="card mx-auto m-5">
-                    <div class="card-body">
-                        <h5 class="card-title"> اضافه کردن پست </h5>
-                        <p> پست مورد نظر خود را ویرایش کنید </p>
-                        <form method="POST">
+        <div class="card mx-auto m-5">
+            <div class="card-body">
+                <h5 class="card-title"> اضافه کردن پست </h5>
+                <form method="POST" action="<?= url_view_builder('admin/addPost') ?>" enctype="multipart/form-data">
 
-                            <div class="mb-3">
-                                <label for="exampleInputUsername" class="form-label"> عنوان </label>
-                                <input name="username" type="text" class="form-control" id="exampleInputUsername"
-                                    aria-describedby="emailHelp" required>
-                            </div>
-                            <div class="mb-3">
-                                <div class="mb-3">
-                                    <label for="exampleFormControlTextarea1" class="form-label"> متن پست را بنویسید </label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                  </div>
-                            </div> 
-
-                            <div class="mb-3">
-                                <label for="formFileMultiple" class="form-label"> تصویر پست </label>
-                                <input class="form-control" type="file" id="formFileMultiple" multiple>
-                              </div>
-                            
-                            <button name="login" type="submit" class="btn btn-primary"> اضافه کردن پست </button>
-                        </form>
+                    <div class="mb-3">
+                        <label for="exampleInputUsername" class="form-label"> عنوان </label>
+                        <input name="title" type="text" class="form-control" id="exampleInputUsername" required>
                     </div>
-                </div>
+                    <div class="mb-3">
+                        <div class="mb-3">
+                            <label for="textPost" class="form-label"> متن پست را بنویسید </label>
+                            <textarea class="form-control" id="textPost" rows="4" name="text" required></textarea>
+                        
+                        </div>
+                        <script>
+                                CKEDITOR.replace('textPost');
+                            </script>
+                    </div>
 
+                    <div class="mb-3">
+                        <label for="formFileMultiple" class="form-label"> تصویر پست </label>
+                        <input class="form-control" type="file" name="image" id="formFileMultiple" multiple>
+                    </div>
+
+                    <button name="send" type="submit" class="btn btn-primary"> اضافه کردن پست </button>
+                </form>
             </div>
         </div>
 
+    </div>
+</div>
 
-<?php 
-    view("inc/footer");
+
+<?php
+view("inc/footer");
 ?>
