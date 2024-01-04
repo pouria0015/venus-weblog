c5b9ebc9e719c4a99df0bc031d2b8e037e60c3ac
pouria0015 <pouria0015@gmail.com>
2024-01-04 12:36:27 +0330

    add: write data post in form edit

--- views/admin/editPosts.php
+++ views/admin/editPosts.php
@@ -1,48 +1,51 @@
-<?php 
-    view("admin/inc/header");
+<?php
+view("admin/inc/header");
 ?>
+<script src="<?= asset('js/ckeditor/ckeditor.js') ?>"></script>
 
-        <div class="d-flex justify-content-center">
-            <div class="row row-cols-1 row-cols-md-1 g-1">
+<div class="d-flex justify-content-center">
+    <div class="row row-cols-1 row-cols-md-1 g-1">
 
-                <div class="card mx-auto m-5">
-                    <img src="https://fakeimg.pl/250x200/?text=slide1&font=lobster" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"> ویرایش پست </h5>
-                        <p> پست را ویرایش کنید </p>
-                        <form method="POST">
+        <div class="card mx-auto m-5">
+            <img src="<?= APPROOT . "/public/img/posts/$data->image" ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> ویرایش پست </h5>
+                <p> پست را ویرایش کنید </p>
+                <form method="POST" action='<?= url_view_builder('admin/editPost/' . $data->id) ?>'>
 
-                            <div class="mb-3">
-                                <label for="exampleInputUsername" class="form-label"> عنوان </label>
-                                <input name="username" type="text" class="form-control" id="exampleInputUsername"
-                                    aria-describedby="emailHelp" required>
-                            </div>
-                            <div class="mb-3">
-                                <div class="mb-3">
-                                    <label for="exampleFormControlTextarea1" class="form-label"> متن پست را بنویسید </label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                  </div>
-                            </div> 
-
-                            <div class="mb-3">
-                                <label for="formFileMultiple" class="form-label"> تصویر پست </label>
-                                <input class="form-control" type="file" id="formFileMultiple" multiple>
-                              </div>
-                            
-                            <button name="login" type="submit" class="btn btn-primary"> ویرایش پست </button>
-                        </form>
-                        <button type="button" class="btn btn-danger m-1"> حذف پست </button>
-
+                    <div class="mb-3">
+                        <label for="exampleInputUsername" class="form-label"> عنوان </label>
+                        <input name="username" type="text" class="form-control" id="exampleInputUsername" value="<?= $data->title ?>">
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted"> تاریخ ایجاد پست  </small>
+                    <div class="mb-3">
+                
+                        <div class="mb-4">
+                            <textarea class="form-control" id="editor1" rows="4" name="body" value="<?= htmlspecialchars_decode(html_entity_decode($data->body)); ?>" ></textarea>
+                            <label class="form-label" for="form4Example3">متن پست را بنویسید </label>
+                        </div>
+                        <script>
+                            CKEDITOR.replace('editor1');
+                        </script>
                     </div>
-                </div>
 
+                    <div class="mb-3">
+                        <label for="formFileMultiple" class="form-label"> تصویر پست </label>
+                        <input class="form-control" type="file" id="formFileMultiple" multiple>
+                    </div>
+
+                    <button name="edit" type="submit" class="btn btn-primary"> ویرایش پست </button>
+                </form>
+
+            </div>
+            <div class="card-footer">
+                <small class="text-muted"> <?= $data->published_at ?>:تاریخ انتشار پست </small>
             </div>
         </div>
 
+    </div>
+</div>
 
-<?php 
-    view("inc/footer");
+
+<?php
+view("inc/footer");
 ?>
