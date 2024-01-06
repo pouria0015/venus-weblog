0e770cc4d8a9bea978cfb1ecc20bd8484813c10a
pouria0015 <pouria0015@gmail.com>
2024-01-06 22:18:29 +0330

    add and edit: add setUserCooke ,removeUserCooke,getIdUser  function and edit loginUser function for add Cooke

--- libraries/Auth/Auth.php
+++ libraries/Auth/Auth.php
@@ -16,10 +16,51 @@ class Auth {
     }
 
     public static function loginUser($user){
+        $status = false;
         if(Session::set('user' , $user)){
-            return true;
+            $status = true;
+            if(isset($user['cooke_token']) && !empty($user['cooke_token']) && !is_null($user['cooke_token'])){
+                
+                self::setUserCooke($user);
+                
+                $status = true;
+                if($status === true){
+                    return true;
+                }
+            }else{
+                return true;
+            }
         }
         return false;
+    }
+
+    private static function setUserCooke($data){
+        $userDataString = implode(':' , 
+                                        [
+                                            'user_id' => $data['id'] ,
+                                            'user_name' => $data['user_name'] ,
+                                            'user_email' => $data['email'],
+                                            'cooke_token' => $data['cooke_token']
+                                        ]);
+                                
+        setcookie('Account' , $userDataString ,time() + 3600 * 24 ,"/" , "" , false , true);
+        if(isset($_COOKIE['Account'])){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function removeUserCooke(){
+        
+        if(!empty($_COOKIE['Account']) && isset($_COOKIE['Account'])){
+        if(setcookie('Account' , "" ,time() - 1 ,"/" , "" , false , true)){
+            return true;
+        }else{
+            return false;
+        }
+        }
+        return true;
     }
 
     public static function logoutUser(){
@@ -28,6 +69,11 @@ class Auth {
 
     public static function getLoggedInUser(){
         return Session::get('user');
+    }
+
+    public static function getIdUser(){
+        $userData  = Session::get('user');
+        return $userData['id'];
     }
 
     public static function isAuthenticated(){
