ed6e68963ef9a9868b26c0503f21974f037d4bc2
pouria0015 <pouria0015@gmail.com>
2024-01-19 22:39:16 +0330

    add: check enable cookie in user browser and add exit and show alert for not enable cookie

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 require_once "../bootstrap.php";
 
+$browser = get_browser(null , true);
+$browser['cookies'] == 0 ? exit('<small>Enable the use of cookies for the venus blog</small>') : '';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
