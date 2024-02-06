0af97f550e7527cde7def72eb8a72eaa8b6fd380
pouria0015 <pouria0015@gmail.com>
2024-02-06 18:28:35 +0330

    remove: check cookie unable

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 require_once "../bootstrap.php";
 
-$browser = get_browser(null , true);
-$browser['cookies'] == 0 ? exit('<small>Enable the use of cookies for the venus blog</small>') : '';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
