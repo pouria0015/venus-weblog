95689a0efcc2cbbe98eb47e5695a371a4b959b0c
pouria0015 <pouria0015@gmail.com>
2024-02-27 22:27:20 +0330

    add:code js for search box

--- views/inc/footer.php
+++ views/inc/footer.php
@@ -13,6 +13,22 @@
     
     <!-- add javascript bundle for bootstrap --> 
     <script src="<?= asset("js/bootstrap.bundle.min.js"); ?>"></script>
+    <script>
+document.querySelector(".search input").addEventListener("keyup", function() {
+  let input = document.querySelector(".search input");
+  const filter = input.value.toLowerCase();
+
+  const posts = document.querySelectorAll(".main_content");
+  for(post of posts) {
+    
+    let title = post.querySelector('.card-title');
+
+    let found = title.innerText.toLowerCase().search(filter);
+    console.log(post);
+    post.style.display = (found === -1) ? "block" : "none";
+  }
+});
+    </script>
    
  </body>
     
