791c291d82d1d3920201d42df16911bbd70fbd39
pouria0015 <pouria0015@gmail.com>
2024-02-28 20:39:10 +0330

    add: function set and get jwt

--- libraries/Auth/Auth.php
+++ libraries/Auth/Auth.php
@@ -7,6 +7,9 @@
 namespace Libraries\Auth;
 
 use Libraries\Session\Session;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 
 class Auth {
 
@@ -19,32 +22,37 @@ class Auth {
         $status = false;
         if(Session::set('user' , $user)){
             $status = true;
-            
+
             if(isset($user['cookie_token']) && !is_null($user['cookie_token']) && $user['cookie_token'] != "" && $user['cookie_token'] != 0){
-       
-                self::setUserCooke($user);
-                
-                $status = true;
+   
+                $res = self::setJwt($user);
+                if(!is_null($res)){
+                    self::setUserCooke($res);
                 if($status === true){
+
                     return true;
+                
                 }
+            }
             }else{
+
                 return true;
+
             }
         }
         return false;
     }
 
     private static function setUserCooke($data){
-        $userDataString = implode(':' , 
-                                        [
-                                            'user_id' => $data['id'] ,
-                                            'user_name' => $data['user_name'] ,
-                                            'user_email' => $data['email'],
-                                            'cookie_token' => $data['cookie_token']
-                                        ]);
+        // $userDataString = implode(':' , 
+        //                                 [
+        //                                     'user_id' => $data['id'] ,
+        //                                     'user_name' => $data['user_name'] ,
+        //                                     'user_email' => $data['email'],
+        //                                     'cookie_token' => $data['cookie_token']
+        //                                 ]);
                                 
-        setcookie('Account' , $userDataString ,time() + 3600 * 24 ,"/" , "" , false , true);
+        setcookie('Account' , $data ,time() + 3600 * 24 ,"/" , "" , false , true);
         if(isset($_COOKIE['Account'])){
             return true;
         }else{
@@ -53,7 +61,29 @@ class Auth {
     }
 
     public static function getDataCooke(){
-        return explode(':' , $_COOKIE['Account']);
+        return self::getDataJwt();
+    }
+
+    public function getDataJwt(){
+        
+        $token = JWT::decode($_COOKIE['Account'] , new Key(SECRET_KEY, 'HS256'));
+
+
+        if (
+            $token->iss !== URLROOT ||
+            $token->iat > time() ||
+            $token->exp < time()
+        ) {
+
+            return false;
+
+        }else{
+
+            return explode(':' , $token->data);
+
+        }
+
+
     }
 
     public static function removeUserCooke(){
@@ -101,6 +131,31 @@ class Auth {
         }elseif((self::getLoggedInUser()['user_type']) !== "admin" && (self::getLoggedInUser()['user_type']) !== "writer"){
                 redirect("");
         }
+    }
+
+
+    public static function setJwt($data){
+
+        $secretKey = SECRET_KEY;
+        $userDataString = implode(':' , 
+        [
+            'user_id' => $data['id'] ,
+            'user_name' => $data['user_name'] ,
+            'user_email' => $data['email'],
+            'cookie_token' => $data['cookie_token']
+        ]);
+        $token = [
+            'iss' => 'URLROOT',
+            'aud' => 'URLROOT',
+            "iat" => time(),
+            "exp" => time() + (60 * 60 * 24 * 7),
+            "data" => $userDataString
+        ];
+
+
+        $jwt = JWT::encode($token , $secretKey , "HS256");
+        return $jwt;
+
     }
 
 }
