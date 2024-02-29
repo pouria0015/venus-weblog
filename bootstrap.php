caa55d011079dc731ef8d6ecc664e39b5a5cb244
pouria0015 <pouria0015@gmail.com>
2024-02-29 21:29:18 +0330

    edit: comment include files phpmailer and add nameSpace for phpmailer

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once("../vendor/autoload.php");
-require '../vendor/PHPMailer/src/Exception.php';
-require '../vendor/PHPMailer/src/PHPMailer.php';
-require '../vendor/PHPMailer/src/SMTP.php';
+// require '../vendor/PHPMailer/src/Exception.php';
+// require '../vendor/PHPMailer/src/PHPMailer.php';
+// require '../vendor/PHPMailer/src/SMTP.php';
 require_once("../config/config.php");
 require_once("../helpers/url_helper.php");
 require_once("../helpers/flash_helper.php");
 require_once("../helpers/helper.php");
 require_once("../helpers/session_helper.php");
+require_once("../helpers/mail_helper.php");
